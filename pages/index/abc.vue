87a2dd9db622b6eb3d327b2e004064dc7f88dc6b
Buiducthang123 <winnerbui0803@gmal.com>
2024-11-14 22:32:26 +0700

    fix

--- pages/index/abc.vue
+++ pages/index/abc.vue
@@ -1,43 +1,26 @@
 <template>
   <div>
-    <h1>Upload Image</h1>
-    <input type="file" @change="onFileChange" />
-    <button @click="uploadImage">Upload</button>
-
-    <Ckeditor v-model:value="a"/>
+    <button @click="exportToExcel">Xuất Excel</button>
   </div>
 </template>
 
-<script lang="ts" setup>
+<script setup>
 import { ref } from 'vue';
-const a = ref('')
-const selectedFile = ref<File | null>(null);
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
 
-const onFileChange = (event: Event) => {
-  const target = event.target as HTMLInputElement;
-  if (target.files && target.files[0]) {
-    selectedFile.value = target.files[0];
-  }
-};
+const data = ref([
+  { id: 1, name: 'John Doe', email: 'john@example.com' },
+  { id: 2, name: 'Jane Doe', email: 'jane@example.com' },
+]);
 
-const uploadImage = async () => {
-  if (!selectedFile.value) {
-    alert('Please select a file first.');
-    return;
-  }
+const exportToExcel = () => {
+  const worksheet = XLSX.utils.json_to_sheet(data.value);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
 
-  const formData = new FormData();
-  formData.append('image', selectedFile.value);
-
-  try {
-    const response = await fetch('https://api.imgbb.com/1/upload?key=14cc7136f05a039beb83d8183385e78b', {
-      method: 'POST',
-      body: formData,
-    });
-    const data = await response.json();
-    console.log('Image uploaded successfully:', data);
-  } catch (error) {
-    console.error('Error uploading image:', error);
-  }
+  const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+  const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
+  saveAs(blob, 'output.xlsx');
 };
 </script>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
     <Loading v-if="!statusResponse" />
-    <div  v-else class="admin-dashboard p-6 bg-gray-100 min-h-screen">
+    <div v-else class="admin-dashboard p-6 bg-gray-100 min-h-screen">
         <h1 class="text-3xl font-bold mb-6">Thống kê</h1>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
             <div class="info-box bg-white p-4 rounded-lg shadow-md">
@@ -11,12 +11,22 @@
                 <h2 class="text-lg font-semibold mb-2">Tổng số phòng</h2>
                 <p class="text-2xl font-bold">{{ statusResponse?.totalRoom }}</p>
             </div>
+            <div class="info-box bg-white p-4 rounded-lg shadow-md">
+                <h2 class="text-lg font-semibold mb-2">Tổng doanh thu</h2>
+                <p class="text-2xl font-bold">{{ formatCurrency(statusResponse?.totalRevenue) }}</p>
+            </div>
         </div>
         <div class="mb-8 flex items-center gap-4">
-            <a-select v-model:value="selectedRange" @change="handleChange" class="w-48">
-                <a-select-option value="all">Từ trước tới nay</a-select-option>
+            <a-select v-model:value="selectedYear" @change="handleYearChange" class="w-48">
                 <a-select-option v-for="year in lastFiveYears" :key="year" :value="year">{{ year }}</a-select-option>
             </a-select>
+            <a-select v-model:value="selectedMonth" @change="handleMonthChange" class="w-48">
+                <a-select-option v-for="month in months" :key="month.value" :value="month.value">{{ month.label }}</a-select-option>
+            </a-select>
+
+            <div>
+                <a-button type="primary" @click="handleDownloadCSV">Xuất csv doanh thu</a-button>
+            </div>
         </div>
         <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div class="chart-container bg-white p-6 rounded-lg shadow-md">
@@ -40,7 +50,7 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import { useAuthStore } from '~/stores/auth';
 import { useRuntimeConfig } from '#app';
 import {
@@ -57,7 +67,10 @@ import {
 } from 'chart.js';
 import { Pie, Bar, Line } from 'vue-chartjs';
 import dayjs from 'dayjs';
+import 'dayjs/locale/vi';
+import localeData from 'dayjs/plugin/localeData';
 import type { SelectProps } from 'ant-design-vue';
+
 // Register Chart.js components
 ChartJS.register(
     CategoryScale,
@@ -70,6 +83,10 @@ ChartJS.register(
     Tooltip,
     Legend
 );
+
+dayjs.locale('vi');
+dayjs.extend(localeData);
+
 const chartOptions = ref({
     responsive: true,
     maintainAspectRatio: false,
@@ -90,23 +107,42 @@ const token = computed(() => authStore.accessToken);
 //selected range
 const currentYear = dayjs().year();
 const lastFiveYears = Array.from({ length: 5 }, (_, i) => currentYear - i);
+const months = [
+    { value: 'all', label: 'Tất cả các tháng' },
+    ...Array.from({ length: 12 }, (_, i) => ({
+        value: i + 1,
+        label: dayjs().month(i).format('MMMM')
+    }))
+];
 
-const selectedRange = ref('all');
+const selectedYear = ref<any>(currentYear);
+const selectedMonth = ref<any>('all');
+
 const rangeTimeSelected = ref<{
     start: string | null,
     end: string | null
-}>({
-    start: '08/03/2002',
-    end: dayjs().format('DD/MM/YYYY')
-});
-const handleChange: SelectProps['onChange'] = value => {
-    if (value === 'all') {
-    rangeTimeSelected.value.start = '08/03/2002';
-    rangeTimeSelected.value.end = dayjs().format('DD/MM/YYYY');
-  } else {
-    rangeTimeSelected.value.start = dayjs().year(Number(value)).startOf('year').format('DD/MM/YYYY');
-    rangeTimeSelected.value.end = dayjs().year(Number(value)).endOf('year').format('DD/MM/YYYY');
-  }
+}>(getRange(selectedYear.value, selectedMonth.value));
+
+function getRange(year: number, month: string | number) {
+    if (month === 'all') {
+        const start = dayjs().year(year).startOf('year').format('DD/MM/YYYY');
+        const end = dayjs().year(year).endOf('year').format('DD/MM/YYYY');
+        return { start, end };
+    } else {
+        const start = dayjs().year(year).month(Number(month) - 1).startOf('month').format('DD/MM/YYYY');
+        const end = dayjs().year(year).month(Number(month) - 1).endOf('month').format('DD/MM/YYYY');
+        return { start, end };
+    }
+}
+
+const handleYearChange: SelectProps['onChange'] = value => {
+    selectedYear.value = value;
+    rangeTimeSelected.value = getRange(selectedYear.value, selectedMonth.value); 
+};
+
+const handleMonthChange: SelectProps['onChange'] = value => {
+    selectedMonth.value = value;
+    rangeTimeSelected.value = getRange(selectedYear.value, selectedMonth.value);
 };
 
 //fetch data
@@ -119,10 +155,53 @@ const { data: statusResponse, refresh } = useFetch<any>('/api/statistical', {
     },
     baseURL: useRuntimeConfig().public.baseURL,
     query: {
-        range: rangeTimeSelected.value
+        // range: rangeTimeSelected.value
+        range: rangeTimeSelected
+
     },
-    watch: [selectedRange],
+    watch: [rangeTimeSelected],
 });
+
+const formatCurrency = (value: number) => {
+    return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(value);
+};
+
+const handleDownloadCSV = async () => {
+    try {
+        const range = {
+            start: rangeTimeSelected.value.start,
+            end: rangeTimeSelected.value.end
+        };
+        const encodedRange = encodeURIComponent(JSON.stringify(range));
+
+        const response = await fetch(`${useRuntimeConfig().public.baseURL}api/statistical/export?range=${encodedRange}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token.value}`
+            }
+        });
+
+        if (response.ok) {
+            const blob = await response.blob();
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'statistical_export.xlsx';
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+            window.URL.revokeObjectURL(url);
+
+            message.success('Tải về thành công');
+        } else {
+            message.error('Có lỗi xảy ra');
+        }
+    } catch (error) {
+        console.error("Download CSV error:", error);
+        message.error('Có lỗi xảy ra khi tải file');
+    }
+};
+
 
 onMounted(() => {
     refresh();
@@ -134,11 +213,9 @@ onMounted(() => {
     padding: 20px;
 }
 
-
 .chart-container {
     width: 100%;
     height: 400px;
-    /* background-color: #fff; */
     padding: 20px;
     border-radius: 8px;
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
